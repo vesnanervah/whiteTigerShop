3475e3f543147344e7ad5c11daf76a1f1ce5f9f8
vesnanervah <c.zwerew2012@gmail.com>
2023-12-08 23:00:32 +0300

    feat: add list sort options in products list page

--- white_tiger_shop/lib/types/types.dart
+++ white_tiger_shop/lib/types/types.dart
@@ -2,3 +2,4 @@ typedef ApiArgs = Map<String, String>;
 typedef CategoriesInnerRespData = Map<String, dynamic>;
 typedef ProductsInnerRespData = List<dynamic>;
 typedef DetailedProductInnerRespData = Map<String, dynamic>;
+typedef SortEvents = List<Function>;
--- white_tiger_shop/lib/widgets/products_grid_page.dart
+++ white_tiger_shop/lib/widgets/products_grid_page.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:white_tiger_shop/models/categories_model.dart';
 import 'package:white_tiger_shop/models/products_model.dart';
+import 'package:white_tiger_shop/types/types.dart';
 import 'package:white_tiger_shop/widgets/detailed_product_page.dart';
 import 'package:white_tiger_shop/widgets/products_list_item.dart';
 
@@ -14,9 +17,15 @@ class ProductsGridPage extends StatefulWidget {
 
 class _ProductsGridPageState extends State<ProductsGridPage> {
   final model = ProductsModel();
+  List<Product>? products;
+  List<bool> selectedBtns = [false, false];
   @override
   Widget build(BuildContext context) {
-    model.fetchProducts(widget.category.categoryId);
+    List<Function> events = [
+      model.getSortedByName,
+      model.getSortedByPrice,
+    ]; //сомнительное решение
+    if (model.products == null) model.fetchProducts(widget.category.categoryId);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category.title),
@@ -27,22 +36,77 @@ class _ProductsGridPageState extends State<ProductsGridPage> {
         child: ListenableBuilder(
           listenable: model,
           builder: (BuildContext context, Widget? child) {
-            return model.products == null
+            if (model.products != null && products == null) {
+              products = model.products!.toList();
+            }
+            return products == null
                 ? const Center(child: CircularProgressIndicator())
-                : ListView.separated(
-                    itemBuilder: (context, index) => ProductsItemView(
-                          model.products![index],
-                          () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) =>
-                                  DetailedProductPage(model.products![index]),
+                : Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(
+                            left: 15, bottom: 10, top: 15),
+                        child: ToggleButtons(
+                          isSelected: selectedBtns,
+                          onPressed: (index) {
+                            setState(() {
+                              if (!selectedBtns[index]) {
+                                products = events[index]();
+                                products!.forEach((element) {
+                                  log('${element.price}');
+                                });
+                              } else {
+                                products = model.products!.toList();
+                              }
+                              for (var i = 0; i < selectedBtns.length; i++) {
+                                if (i == index) {
+                                  selectedBtns[i] = !selectedBtns[i];
+                                } else {
+                                  selectedBtns[i] = false;
+                                }
+                              }
+                            });
+                          },
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.only(left: 5),
+                              width: 140,
+                              child: const Row(children: [
+                                Icon(Icons.sort_by_alpha),
+                                Padding(padding: EdgeInsets.only(right: 5)),
+                                Text('По названию')
+                              ]),
                             ),
-                          ),
+                            Container(
+                              padding: const EdgeInsets.only(left: 5),
+                              width: 140,
+                              child: const Row(children: [
+                                Icon(Icons.price_change),
+                                Padding(padding: EdgeInsets.only(right: 5)),
+                                Text('По цене')
+                              ]),
+                            ),
+                          ],
                         ),
-                    separatorBuilder: (context, index) =>
-                        const Divider(height: 10),
-                    itemCount: model.products!.length);
+                      ),
+                      Expanded(
+                        child: ListView.separated(
+                            itemBuilder: (context, index) => ProductsItemView(
+                                  products![index],
+                                  () => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (_) =>
+                                          DetailedProductPage(products![index]),
+                                    ),
+                                  ),
+                                ),
+                            separatorBuilder: (context, index) =>
+                                const Divider(height: 10),
+                            itemCount: model.products!.length),
+                      ),
+                    ],
+                  );
           },
         ),
       ),
