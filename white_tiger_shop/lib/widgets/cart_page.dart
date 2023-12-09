7eb4ba2c9acb37b241a2fc86afcd3da8a7979c60
vesnanervah <c.zwerew2012@gmail.com>
2023-12-09 22:25:08 +0300

    feat: add routing to detailed page from cart

--- white_tiger_shop/lib/widgets/cart_page.dart
+++ white_tiger_shop/lib/widgets/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:white_tiger_shop/main.dart';
+import 'package:white_tiger_shop/widgets/detailed_product_page.dart';
 import 'package:white_tiger_shop/widgets/products_list_item.dart';
 import 'package:white_tiger_shop/widgets/wtshop_app_bar.dart';
 
@@ -11,40 +12,47 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final state = context.watch<AppState>();
     return Scaffold(
-        appBar: const WtShopAppBar('Корзина'),
-        body: Container(
-          color: Colors.black12,
-          child: ListenableBuilder(
-            listenable: state.cart,
-            builder: (_, widget) {
-              final productsIds = state.cart.products.keys.toList();
-              return productsIds.isNotEmpty
-                  ? ListView.separated(
-                      // widget with delete from cart button
-                      itemBuilder: (_, index) {
-                        return ProductsItemView(
-                          state.cart.products[productsIds[index]]!,
-                          () {
-                            // TODO: navigate to detailed page
+      appBar: const WtShopAppBar('Корзина'),
+      body: Container(
+        color: Colors.black12,
+        child: ListenableBuilder(
+          listenable: state.cart,
+          builder: (_, widget) {
+            final productsIds = state.cart.products.keys.toList();
+            return productsIds.isNotEmpty
+                ? ListView.separated(
+                    // widget with delete from cart button
+                    itemBuilder: (_, index) {
+                      return ProductsItemView(
+                        state.cart.products[productsIds[index]]!,
+                        () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => DetailedProductPage(
+                                  state.cart.products[productsIds[index]]!),
+                            ),
+                          );
+                        },
+                        trailing: IconButton(
+                          icon: const Icon(Icons.close),
+                          tooltip: 'Убрать из корзины',
+                          onPressed: () {
+                            state.cart.removeFromCart(
+                                state.cart.products[productsIds[index]]!);
                           },
-                          trailing: IconButton(
-                            icon: const Icon(Icons.close),
-                            tooltip: 'Убрать из корзины',
-                            onPressed: () {
-                              state.cart.removeFromCart(
-                                  state.cart.products[productsIds[index]]!);
-                            },
-                          ),
-                        );
-                      },
-                      separatorBuilder: (_, index) => const Divider(height: 10),
-                      itemCount: state.cart.getLen(),
-                    )
-                  : const Center(
-                      child: Text('В корзине ничего нет'),
-                    );
-            },
-          ),
-        ));
+                        ),
+                      );
+                    },
+                    separatorBuilder: (_, index) => const Divider(height: 10),
+                    itemCount: state.cart.getLen(),
+                  )
+                : const Center(
+                    child: Text('В корзине ничего нет'),
+                  );
+          },
+        ),
+      ),
+    );
   }
 }
