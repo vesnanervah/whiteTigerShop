c03b7c526703b5b1fab59afbe87714953f8f0a21
vesnanervah <c.zwerew2012@gmail.com>
2023-12-15 19:15:12 +0300

    refactor: detailed product

--- white_tiger_shop/lib/product/detailed_product_page.dart
+++ white_tiger_shop/lib/product/detailed_product_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:white_tiger_shop/common/view/base_page.dart';
+import 'package:white_tiger_shop/common/view/networked_image.dart';
+import 'package:white_tiger_shop/main.dart';
 import 'package:white_tiger_shop/product/model/detailed_product_model.dart';
 import 'package:white_tiger_shop/product/model/entity/product.dart';
-import 'package:white_tiger_shop/product/view/detailed_product_view.dart';
 
 class DetailedProductPage extends StatefulWidget {
   final Product product;
@@ -18,6 +20,18 @@ class _DetailedProductPageState extends State<DetailedProductPage> {
 
   @override
   Widget build(BuildContext context) {
+    final state = context.watch<AppState>();
+
+    double calculateCardWidth(BuildContext context) {
+      final width = MediaQuery.of(context).size.width;
+      if (width < 575) {
+        return width - 30;
+      } else if (width >= 575 && width < 768) {
+        return 360;
+      }
+      return 440;
+    }
+
     return BasePage(
       model,
       widget.product.title,
@@ -25,7 +39,68 @@ class _DetailedProductPageState extends State<DetailedProductPage> {
         model.productId = widget.product.productId;
         model.update();
       },
-      () => DetailedProductView(model.data!),
+      () => Center(
+        child: SingleChildScrollView(
+          child: Card(
+            child: Container(
+              constraints:
+                  BoxConstraints(maxWidth: calculateCardWidth(context)),
+              padding: const EdgeInsets.only(
+                  top: 15, bottom: 20, left: 10, right: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Center(child: NetworkedImage(180, 180, model.data!.imageUrl)),
+                  const SizedBox(height: 15),
+                  Text(
+                    model.data!.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 19,
+                      height: 1,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    'Цена: ${model.data!.price}',
+                    style: const TextStyle(fontSize: 16, height: 1.5),
+                  ),
+                  Text(
+                    'Категория: ${model.data!.category ?? 'Не предоставлено'}',
+                    style: const TextStyle(fontSize: 16, height: 1.5),
+                  ),
+                  Text(
+                    'Описание: ${model.data!.productDescription != null && model.data!.productDescription!.isNotEmpty ? model.data!.productDescription! : 'Не предоставлено'}',
+                    style: const TextStyle(fontSize: 16, height: 1.5),
+                  ),
+                  const SizedBox(height: 10),
+                  Center(
+                    child: ListenableBuilder(
+                      listenable: state.cart,
+                      builder: (_, widget) {
+                        return ElevatedButton(
+                          // add styles according to cart changes
+                          onPressed: () {
+                            if (state.cart.inCart(model.data!)) {
+                              state.cart.removeFromCart(model.data!);
+                            } else {
+                              state.cart.addToCart(model.data!);
+                            }
+                          },
+                          child: state.cart.inCart(model.data!)
+                              ? const Text('В корзине')
+                              : const Text('В корзину'),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
