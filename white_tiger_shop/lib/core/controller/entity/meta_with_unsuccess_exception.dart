de788764a35b7419c406527b7c1c481b1d7c7228
vesnanervah <c.zwerew2012@gmail.com>
2023-12-23 21:58:43 +0300

    clean up

--- white_tiger_shop/lib/core/controller/entity/meta_with_unsuccess_exception.dart
+++ white_tiger_shop/lib/core/controller/entity/meta_with_unsuccess_exception.dart
@@ -3,5 +3,6 @@ import 'package:white_tiger_shop/core/controller/entity/base_response.dart';
 class MetaWithUnsuccesException implements Exception {
   String errorMsg;
   BaseResp? resp;
+
   MetaWithUnsuccesException(this.errorMsg, {this.resp});
 }
