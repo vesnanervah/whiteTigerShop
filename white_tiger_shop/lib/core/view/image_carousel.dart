35d361ccb3dcd08519033d5215814171a03434f0
vesnanervah <c.zwerew2012@gmail.com>
2024-01-08 16:34:34 +0300

    feat: add swipe animation on longtap drag

--- white_tiger_shop/lib/core/view/image_carousel.dart
+++ white_tiger_shop/lib/core/view/image_carousel.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:white_tiger_shop/core/view/my_colors.dart';
 import 'package:white_tiger_shop/core/view/networked_image.dart';
@@ -16,20 +14,44 @@ class ImageCarousel extends StatefulWidget {
 class _ImageCarouselState extends State<ImageCarousel> {
   var selectedIndex = 0;
   var swipeStart = 0.0;
+  bool? direction;
 
   void selectImage(int index) {
     if (index == selectedIndex) return;
-    setState(() => selectedIndex = index);
+    setState(() {
+      selectedIndex = index;
+      direction = null;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        // TODO: Wrap with transform and make animation on swipe start
         GestureDetector(
           onLongPressStart: (details) {
             swipeStart = details.localPosition.dx;
+          },
+          onLongPressMoveUpdate: (details) {
+            final delta = swipeStart - details.localPosition.dx;
+            if ((delta < 0 && selectedIndex == 0) ||
+                (delta > 0 && selectedIndex == widget.urls.length - 1)) {
+              // кейс когда мы пытаемся двинуть карусель вправо, а картинок слева больше нет
+              // и наоборот
+              return;
+            }
+            if ((delta > 0.0 && delta < 50) ||
+                (delta < 0.0 && delta > -50.0) ||
+                delta == 0.0) {
+              // кейс когда юзер хотел свайпнуть, но передумал, либо прокрутил карусель недостаточно
+              setState(() {
+                direction = null;
+              });
+              return;
+            }
+            setState(() {
+              direction = delta > 0;
+            });
           },
           onLongPressEnd: (details) {
             var swipeEnd = details.localPosition.dx;
@@ -41,13 +63,18 @@ class _ImageCarouselState extends State<ImageCarousel> {
               selectImage(selectedIndex - 1);
             }
           },
-          child: IndexedStack(
-            index: selectedIndex,
-            children: widget.urls.isNotEmpty
-                ? widget.urls
-                    .map((url) => NetworkedImage(200, 200, url))
-                    .toList()
-                : [const NetworkedImage(200, 200, null)],
+          child: AnimatedSlide(
+            duration: const Duration(milliseconds: 120),
+            offset:
+                Offset(direction != null ? (direction! ? -0.2 : 0.2) : 0, 0),
+            child: IndexedStack(
+              index: selectedIndex,
+              children: widget.urls.isNotEmpty
+                  ? widget.urls
+                      .map((url) => NetworkedImage(200, 200, url))
+                      .toList()
+                  : [const NetworkedImage(200, 200, null)],
+            ),
           ),
         ),
         const SizedBox(
